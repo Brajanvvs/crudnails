fe24d2116ed840e0f308850d0f5c0991210f3a97
brajanvvs <brajan.velasquez@uniminuto.edu.co>
2026-03-16 22:42:52 -0500

    fix railway route

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,11 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+const pool = require("./db");
 
-const servicesRoutes = require("./routes/services");
-const authRoutes = require("./routes/auth");
-const appointmentsRoutes = require("./routes/appointments");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-/* SERVIR FRONTEND */
-
-app.use(express.static(path.join(__dirname, "../frontend")));
-
-/* API */
-
-app.use("/services", servicesRoutes);
-app.use("/auth", authRoutes);
-app.use("/appointments", appointmentsRoutes);
-
-app.listen(3000, () => {
-    console.log("Servidor corriendo en puerto 3000");
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error DB");
+  }
 });
